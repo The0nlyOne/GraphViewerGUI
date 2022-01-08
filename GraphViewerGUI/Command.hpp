8c5ff2a1a9c9a6f0ffb5653ff7f420725fb23f3a
The0nlyOne <homernt@outlook.com>
2022-01-08 17:51:09 -0500

    removed some useless comment

--- GraphViewerGUI/Command.hpp
+++ GraphViewerGUI/Command.hpp
@@ -199,14 +199,12 @@ namespace Model
 		ClearGraph(GraphViewer* graphViewer, graph_sptr graphToClear, graph_sptr graphToSwapWith) :
 			graphViewer_(graphViewer), graphToClear_(graphToClear), graphToSwapWith_(graphToSwapWith) {}
 		void execute() override {
-			//graphToSwapWith_ = std::make_shared<Graph>(graphToClear_->getName());
 			graphViewer_->clearGraph(graphToSwapWith_);
 		}
 		void cancel() override {
 			graphViewer_->clearGraph(graphToClear_);
 		}
 		void redo() override {
-			//graphToSwapWith_ = std::make_shared<Graph>(graphToClear_->getName());
 			graphViewer_->clearGraph(graphToSwapWith_);
 		}
 	private:
